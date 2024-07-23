b7e709f0a988de5cb5aabb5b77131b1d11221817
Sarffk17 <Ksarff17@gmail.com>
2024-07-22 21:56:44 -0400

    Updated JS to guess the local time

--- src/script.js
+++ src/script.js
@@ -37,7 +37,9 @@ moscowTimeElement.innerHTML = moscowTime.format("h:mm:ss [<small>]A[</small>]")
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
-
+    if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
