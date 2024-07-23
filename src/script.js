c915c9ca775e312abe25b726f36d5cba318ada65
Sarffk17 <Ksarff17@gmail.com>
2024-07-22 22:04:10 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -50,6 +50,7 @@ function updateCity(event) {
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small> ${cityTime.format("A")}</small></div>
+    </div>
     <a href="index.html">Return to Homepage</a>
     `;
 }
